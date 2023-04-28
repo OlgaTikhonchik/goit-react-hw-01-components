a82f08efc2b0a03c5434ca8b5880bc1fe8cc71f1
OlgaTikhonchik <olga882011@gmail.com>
2023-04-28 11:46:07 +0300

    Верстка и стилизация Statistics

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+import color from './getRandomHexColor';
+
+export const Statistics = ({ stats, title }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.stat__list}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            className={css.item}
+            key={id}
+            style={{ backgroundColor: color() }}
+          >
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    percentage: PropTypes.number.isRequired,
+  }),
+};
